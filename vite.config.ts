c993188967d37c3ac300d165f516ee9dc64da9d7
TWINKLINGPEETALS <twinklingpetalspreschool@gmail.com>
2025-12-14 12:32:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TPSS/",   // 👈 THIS LINE FIXES WHITE SCREEN
-});
+  base: "/TPSS/", // <--- Add this line exactly like this
+})
